fb8d2fa2c445c7fd8ecad6b6495f2ea4127b66fd
Danil Zaviryukha <logolevel@gmail.com>
2016-12-28 22:25:47 +0200

    create template

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function App() {
+	return (
+		<main>
+			<header>
+				<h1>React Todo</h1>
+			</header>
+
+			<section className="todo-list">
+				<div className="todo completed">
+					<button className="checkbox icon">
+						<i className="material-icons">check_box</i>
+					</button>
+
+					<span className="todo-title">Изучить JavaScript</span>
+
+					<button className="delete icon">
+						<i className="material-icons">delete</i>
+					</button>
+				</div>
+				<div className="todo">
+					<button className="checkbox icon">
+						<i className="material-icons">check_box_outline_blank</i>
+					</button>
+
+					<span className="todo-title">Изучить React</span>
+
+					<button className="delete icon">
+						<i className="material-icons">delete</i>
+					</button>
+				</div>
+			</section>
+		</main>
+	);
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
